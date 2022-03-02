e79efac9d70456c87c79af57d2d9eec330fffa5d
LuisAHC <luishceciliano@gmail.com>
2022-03-02 01:55:19 -0600

    agregados CircuitBreaker y TimeLimmiter

--- Fraudes/src/main/java/com/ibm/academia/fraudes/controladores/AppController.java
+++ Fraudes/src/main/java/com/ibm/academia/fraudes/controladores/AppController.java
@@ -1,13 +1,17 @@
 package com.ibm.academia.fraudes.controladores;
 
+import com.ibm.academia.fraudes.modelo.dto.PaisDTO;
 import com.ibm.academia.fraudes.modelo.entidades.ListaNegra;
 import com.ibm.academia.fraudes.modelo.servicios.IpService;
 import com.ibm.academia.fraudes.modelo.servicios.ListaNegraService;
 import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/cliente")
@@ -38,20 +43,31 @@ public class AppController {
      * @return Retorna un objeto con información del país
      * @author LAHC 27-02-2022
      */
+    @CircuitBreaker(name = "info", fallbackMethod = "metodoAlternativo")
+    @TimeLimiter(name = "info")
     @GetMapping("/buscar_info/{ip}")
-    public ResponseEntity<?> buscarInfoIp(@PathVariable String ip){
+    public CompletableFuture<ResponseEntity<?>> buscarInfoIp(@PathVariable String ip){
         Optional<ListaNegra> estadoIp = listaNegraService.encontrarPorIp(ip);
 
         if (estadoIp.isEmpty()){
-            Map<String, Object> respuesta = new HashMap<>();
-            try {
-                return new ResponseEntity<>(ipService.obtenerInfoIp(ip), HttpStatus.OK);
-            }catch (FeignException fe){
-                logger.info("mensaje: " + fe.getMessage() + " causa: " + fe.getCause());
-                respuesta.put("mensaje", fe.getMessage());
-                return new ResponseEntity<>(respuesta, HttpStatus.NOT_FOUND);
-            }
+            return CompletableFuture.supplyAsync((() -> new ResponseEntity<>(ipService.obtenerInfoIp(ip), HttpStatus.OK) ));
         }else
-            return new ResponseEntity<>("La IP ingresada se encuentra en lista negra", HttpStatus.BAD_REQUEST);
+            return CompletableFuture.supplyAsync((() -> new ResponseEntity<>("La IP ingresada se encuentra en lista negra", HttpStatus.BAD_REQUEST)));
+    }
+
+    /**
+     * Metodo alternativo para controlar problemas en buscarInfoIp()
+     * @param e La excepción que causo el problema
+     * @return Retorna una respuesta al error
+     * @author LAHC 01-03-2022
+     */
+    public CompletableFuture<ResponseEntity<?>> metodoAlternativo(Throwable e) {
+        logger.info("mensaje: " + e.getMessage() + " causa: " + e.getCause());
+        Map<String, Object> respuesta = new HashMap<>();
+        if (e instanceof FeignException){
+            respuesta.put("mensaje", e.getMessage());
+            throw new RuntimeException(respuesta.toString());
+        }
+        return CompletableFuture.supplyAsync((() -> new ResponseEntity<>("No se puede mostrar información, intentalo de nuevo", HttpStatus.BAD_REQUEST)));
     }
 }
--- Fraudes/src/main/java/com/ibm/academia/fraudes/controladores/ListaNegraController.java
+++ Fraudes/src/main/java/com/ibm/academia/fraudes/controladores/ListaNegraController.java
@@ -1,16 +1,20 @@
 package com.ibm.academia.fraudes.controladores;
 
 import com.ibm.academia.fraudes.modelo.servicios.ListaNegraService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/admin")
@@ -25,19 +29,26 @@ public class ListaNegraController {
      * Endpoint para agregar una IP a la lista negra
      * @param ip IP que será agregada a la lista negra
      * @return Retorna mensaje diciendo si la IP fue correctamente agregada a la lista negra
-     * @author LAHC - 28-02-2022
+     * @author LAHC 28-02-2022
      */
+    @CircuitBreaker(name = "ban", fallbackMethod = "metodoAlternativo")
+    @TimeLimiter(name = "ban")
     @PutMapping("/banear/{ip}")
-    public ResponseEntity<?> banearIP(@PathVariable String ip)	{
+    public CompletableFuture<ResponseEntity<?>> banearIP(@PathVariable String ip){
         Map<String, Object> respuesta = new HashMap<>();
         try {
             listaNegraService.banearPorIp(ip);
         } catch (Exception e) {
             logger.info("mensaje: " + e.getMessage() + " causa: " + e.getCause());
-            respuesta.put("mensaje", e.getMessage());
-            return new ResponseEntity<>(respuesta, HttpStatus.BAD_REQUEST);
+            return CompletableFuture.supplyAsync((() -> new ResponseEntity<>("La IP ya se encuentra en la lista negra", HttpStatus.BAD_REQUEST)));
         }
+        return CompletableFuture.supplyAsync((() -> new ResponseEntity<>("IP agregada a la lista negra correctamente", HttpStatus.OK)));
+    }
 
-        return new ResponseEntity<>("IP agregada a la lista negra correctamente", HttpStatus.OK);
+    public CompletableFuture<ResponseEntity<?>> metodoAlternativo(Throwable e) {
+        Map<String, Object> respuesta = new HashMap<>();
+        logger.info("mensaje: " + e.getMessage() + " causa: " + e.getCause());
+        respuesta.put("mensaje", e.getMessage());
+        return CompletableFuture.supplyAsync(() -> new ResponseEntity<>("No se pudo banear la IP, intentalo de nuevo", HttpStatus.BAD_REQUEST));
     }
 }
